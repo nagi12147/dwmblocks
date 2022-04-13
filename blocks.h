eea930c40a0968161ac9f7a19daebae8187f3de4
nagi12147 <nagi12147@tutanota.com>
2022-04-13 11:56:01 +0700

    new script

--- blocks.h
+++ blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon      Command     Update Interval	    Update Signal*/
-	{ " ",      "wifi",      30,                4},
-	{ "",       "wifisignal", 1,                4},
-	{ "",       "netraf",    1,                 4},
-	{ "",       "volume",    0,                 10},
-	{ "",       "battery",   1,                 2},
-	{ "",       "datetime",  30,                1},
+	/*Icon*/    /*Command*/                 /*Update Interval*/     /*Update Signal*/
+	{"",        "status-netraf",            1,                      0},
+	{"",        "status-wifi",              5,                      0},
+	{"",        "status-vol",               0,                      1},
+	{"",        "status-batstat",           1,                      0},
+	{"",        "status-bat",               5,                      0},
+	{"",        "status-fulldate",          60,                     0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
